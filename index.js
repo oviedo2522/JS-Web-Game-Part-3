6e14a6ece6f2327d999c4cd80f9f4f4a0f7b12dd
oviedo2522 <oviedo2522@gmail.com>
2024-03-20 21:34:50 -0700

    New Submission

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-function newImage(url, left, bottom){
+function move(image, left, bottom){
+        image.style.position = 'fixed'
+        image.style.left = left + 'px'
+        image.style.bottom = bottom + 'px'
+        function moveToCoordinates(left, bottom){
+            image.style.left = left + 'pix'
+            image.style.bottom = bottom + 'pix'
+        }
+        return {
+            to: moveToCoordinates
+        }
+    }
+
+    
+function newImage(url){
     let image = document.createElement('img')
     image.src = url
-    image.style.position = 'fixed'
-    image.style.left = left + 'px'
-    image.style.bottom = bottom + 'px'
     document.body.append(image)
     return image
 }
 
-newImage('assets/green-character.gif', 100, 250)
+let greenCharacter = newImage('assets/green-character.gif')
+let moveReturn = move(greenCharacter)
+moveReturn.to(100,250)
 newImage('assets/tree.png', 200, 450)
 newImage('assets/pillar.png', 350, 250)
 newImage('assets/pine-tree.png', 450, 350)
@@ -26,6 +39,7 @@ function newItem(url, left, bottom){
     })
     return item
 }
+
 
 newItem('assets/sword.png', 500, 555)
 newItem('assets/shield.png', 165, 335)
